c8d6ecbef19da1e90df1e74119ee7be79cb9c0bc
Minat Silvester <minatsilvester@gmail.com>
2025-01-19 12:06:40 +0530

    feat: added config for local persistence

---
+++ lib/presistence/user_preferences.dart
@@ -0,0 +1,49 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/user.dart';
+
+class UserPreferences {
+  void saveUser(User user) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString("firstName", user.firstName!);
+    prefs.setString("lastName", user.lastName!);
+    prefs.setString("email", user.email!);
+    prefs.setString("timeZone", user.timeZone!);
+  }
+
+  void saveToken(token) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("token", token);
+  }
+
+  Future<User> getUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? firstName = prefs.getString("firstName");
+    String? lastName = prefs.getString("lastName");
+    String? email = prefs.getString("email");
+    String? timeZone = prefs.getString("timeZone");
+
+    return User(
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        timeZone: timeZone);
+  }
+
+  void removeUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.remove("firstName");
+    prefs.remove("lastName");
+    prefs.remove("email");
+    prefs.remove("timeZone");
+  }
+
+  Future<String> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? token = prefs.getString("token");
+    return token!;
+  }
+}
