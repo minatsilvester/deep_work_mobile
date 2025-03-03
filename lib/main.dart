148c75789b53090fd84891bd4bad843ce3114816
Minat Silvester <minatsilvester@gmail.com>
2025-03-03 21:07:11 +0530

    fix: proper navigation for home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,23 @@ import './screens/register.dart';
 import './theme/default_theme.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const Home()),
-    GoRoute(path: '/sign_up', builder: (context, state) => const Register()),
-    GoRoute(path: '/sign_in', builder: (context, state) => const Login()),
-    GoRoute(
-        path: '/focus_sessions',
-        builder: (context, state) => const FocusSessionList()),
+// final GoRouter _router = GoRouter(initialLocation: '/', routes: [
+//   GoRoute(path: '/', builder: (context, state) => const Home()),
+//   GoRoute(path: '/sign_up', builder: (context, state) => const Register()),
+//   GoRoute(path: '/sign_in', builder: (context, state) => const Login()),
+//   GoRoute(
+//       path: '/focus_sessions',
+//       builder: (context, state) => const FocusSessionList()),
 
-    // 2️⃣ Dynamic route for focus session details
-    GoRoute(
-      path: '/focus_sessions/:id',
-      builder: (context, state) {
-        final id = state.pathParameters['id']!; // Extract id from URL
-        return ShowFocusSession(id: id);
-      },
-    ),
-  ],
-);
+//   // 2️⃣ Dynamic route for focus session details
+//   GoRoute(
+//     path: '/focus_sessions/:id',
+//     builder: (context, state) {
+//       final id = state.pathParameters['id']!; // Extract id from URL
+//       return ShowFocusSession(id: id);
+//     },
+//   ),
+// ]);
 
 void main() {
   runApp(
@@ -52,23 +49,38 @@ class DeepWork extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final auth = Provider.of<AuthProvider>(context);
-
-    // final initialRoute =
-    // auth.loginStatus == Status.loggedIn ? '/focus_sessions' : '/';
-
-    return MaterialApp.router(
-      theme: defaultTheme,
-      routerConfig: _router,
-      debugShowCheckedModeBanner: false,
-      // initialRoute: initialRoute,
-      // routes: {
-      //   '/': (context) => const Home(),
-      //   '/sign_up': (context) => const Register(),
-      //   '/sign_in': (context) => const Login(),
-      //   '/focus_sessions': (context) => const FocusSessionList(),
-      //   // '/focus_sessions/:id': (context) => const ShowFocusSession(id: id)
-      // },
+    return Consumer<AuthProvider>(
+      builder: (context, auth, child) {
+        return MaterialApp.router(
+          theme: defaultTheme,
+          routerConfig: GoRouter(
+            initialLocation: auth.loginStatus == Status.loggedIn
+                ? '/focus_sessions' // Start at focus_sessions if logged in
+                : '/',
+            routes: [
+              GoRoute(path: '/', builder: (context, state) => const Home()),
+              GoRoute(
+                  path: '/sign_up',
+                  builder: (context, state) => const Register()),
+              GoRoute(
+                  path: '/sign_in', builder: (context, state) => const Login()),
+              GoRoute(
+                  path: '/focus_sessions',
+                  builder: (context, state) => const FocusSessionList(),
+                  routes: [
+                    GoRoute(
+                      path: ':id',
+                      builder: (context, state) {
+                        final id = state.pathParameters['id']!;
+                        return ShowFocusSession(id: id);
+                      },
+                    ),
+                  ]),
+            ],
+          ),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
